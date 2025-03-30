35ead93186116432d74011caa6ecae6ced052e88
Myriad-ccc <off7cal@gmail.com>
2025-03-31 01:09:22 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WordCounterVisual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(267, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(349, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Sitka Small", 16F);
+            this.button1.Location = new System.Drawing.Point(299, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Count words";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WordCounterVisual
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = textBox1.Text;
+
+            var words = File.ReadAllText(path).
+                Split(new char[] { ',', ' ', '!', '.', ';', ':', '?', '#', '@' }, StringSplitOptions.RemoveEmptyEntries).
+                Where(x => !string.IsNullOrEmpty(x) && x.All(char.IsLetter)).
+                Select(x => x.ToLower());
+
+            var wordCount = words.GroupBy(word => word);
+
+            Dictionary<string, int> wordFrequency =
+                words.
+                GroupBy(w => w).
+                ToDictionary(g => g.Key, g => g.Count());
+
+            int defaultWidth = -100;
+            int width = defaultWidth, height = 200, index = 1;
+
+            foreach (var entry in wordFrequency.
+                OrderByDescending(value => value.Value).
+                Take(16))
+            {
+                width += 180;
+
+                Label newLabel = new Label()
+                {
+                    Font = new Font("Sitka Small", 14f),
+                    Text = $"{index}.\"{entry.Key}\": {entry.Value}",
+                    Location = new Point(width, height),
+                    AutoSize = true
+                };
+
+                if (ClientSize.Width - width <= 300)
+                {
+                    width = defaultWidth;
+                    height += 65;
+                }
+                index++;    
+
+                Controls.Add(newLabel);
+            }
+        }
+    }
+}
